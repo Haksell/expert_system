f0fe60aafe9fcd532e243c04bb24999c8335640a
Haksell <axel.brisse@gmail.com>
2026-05-30 12:11:13 +0200

    more parse errors (duplicates and invalid order)

--- src/main.rs
+++ src/main.rs
@@ -16,10 +16,20 @@ struct Args {
 enum ParseProgramError {
     IoError(#[expect(unused)] std::io::Error),
     InvalidLine(#[expect(unused)] String),
-    InvalidFact(char),
-    InvalidQuery(char),
+    InvalidFact(#[expect(unused)] char),
+    InvalidQuery(#[expect(unused)] char),
     MissingFacts,
     MissingQueries,
+    DuplicateFacts,
+    DuplicateQueries,
+    QueriesBeforeFacts,
+    RulesAfterFacts,
+}
+
+impl From<std::io::Error> for ParseProgramError {
+    fn from(value: std::io::Error) -> Self {
+        Self::IoError(value)
+    }
 }
 
 #[derive(Debug)]
@@ -28,12 +38,6 @@ struct Rule {}
 impl Rule {
     fn parse(line: &[char]) -> Result<Self, ParseProgramError> {
         Ok(Self {})
-    }
-}
-
-impl From<std::io::Error> for ParseProgramError {
-    fn from(value: std::io::Error) -> Self {
-        Self::IoError(value)
     }
 }
 
@@ -63,18 +67,28 @@ impl Program {
                 continue;
             }
             match line[0] {
-                'A'..='Z' => rules.push(Rule::parse(&line)?),
-                '=' => {
-                    facts = Some(Self::parse_chars(
-                        &line[1..],
-                        ParseProgramError::InvalidFact,
-                    )?)
+                'A'..='Z' => {
+                    if facts.is_some() {
+                        return Err(ParseProgramError::RulesAfterFacts);
+                    }
+                    rules.push(Rule::parse(&line)?);
                 }
+                '=' => Self::parse_letters(
+                    &mut facts,
+                    &line[1..],
+                    ParseProgramError::InvalidFact,
+                    ParseProgramError::DuplicateFacts,
+                )?,
                 '?' => {
-                    queries = Some(Self::parse_chars(
+                    if facts.is_none() {
+                        return Err(ParseProgramError::QueriesBeforeFacts);
+                    }
+                    Self::parse_letters(
+                        &mut queries,
                         &line[1..],
                         ParseProgramError::InvalidQuery,
-                    )?)
+                        ParseProgramError::DuplicateQueries,
+                    )?;
                 }
                 _ => return Err(ParseProgramError::InvalidLine(line.iter().collect())),
             }
@@ -94,18 +108,24 @@ impl Program {
         })
     }
 
-    fn parse_chars(
+    fn parse_letters(
+        letters_to_fill: &mut Option<Vec<char>>,
         line: &[char],
-        error_fn: impl Fn(char) -> ParseProgramError,
-    ) -> Result<Vec<char>, ParseProgramError> {
+        invalid_fn: impl Fn(char) -> ParseProgramError,
+        duplicate_fn: ParseProgramError,
+    ) -> Result<(), ParseProgramError> {
+        if letters_to_fill.is_some() {
+            return Err(duplicate_fn);
+        }
         let mut letters = Vec::with_capacity(line.len());
         for &c in line {
             match c {
                 'A'..='Z' => letters.push(c),
-                _ => return Err(error_fn(c)),
+                _ => return Err(invalid_fn(c)),
             }
         }
-        Ok(letters)
+        letters_to_fill.replace(letters);
+        Ok(())
     }
 }
 
