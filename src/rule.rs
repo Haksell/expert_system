059cdd09cd248cd98ac3060b3effc6a82dc3932c
Haksell <axel.brisse@gmail.com>
2026-06-03 18:57:01 +0200

    mod rule

---
+++ src/rule.rs
@@ -0,0 +1,157 @@
+use crate::{ParseProgramError, Token};
+
+#[derive(Debug)]
+pub enum Rule {
+    Fact(char),
+    Not(Box<Rule>),
+    And(Box<Rule>, Box<Rule>),
+    Or(Box<Rule>, Box<Rule>),
+    Xor(Box<Rule>, Box<Rule>), // TODO: think about Not(Equivalence)
+    Implication(Box<Rule>, Box<Rule>),
+    Equivalence(Box<Rule>, Box<Rule>),
+}
+
+impl Rule {
+    pub fn parse(line: &[char]) -> Result<Self, ParseProgramError> {
+        let tokens = Self::tokenize(line)?;
+        assert!(!tokens.is_empty());
+        let tokens = Self::clean(tokens)?;
+        let tokens = Self::infix_to_rpn(tokens);
+        println!("{tokens:?}");
+        // Self::build(&tokens)
+        Ok(Self::Fact('Z'))
+    }
+
+    fn tokenize(line: &[char]) -> Result<Vec<Token>, ParseProgramError> {
+        let mut tokens = Vec::new();
+        let mut current_token = String::new();
+        for &c in line {
+            if current_token.is_empty() {
+                match c {
+                    'A'..='Z' => tokens.push(Token::Fact(c)),
+                    '(' => tokens.push(Token::LeftParenthesis),
+                    ')' => tokens.push(Token::RightParenthesis),
+                    '!' => tokens.push(Token::Not),
+                    '+' | '&' => tokens.push(Token::And),
+                    '|' => tokens.push(Token::Or),
+                    '^' => tokens.push(Token::Xor),
+                    '=' | '<' => current_token.push(c),
+                    '>' if tokens.last() == Some(&Token::ConverseImplication) => {
+                        if let Some(token) = tokens.last_mut() {
+                            *token = Token::Equivalence;
+                        }
+                    }
+                    _ => {
+                        return Err(ParseProgramError::InvalidToken(
+                            line.iter().collect(),
+                            c.to_string(),
+                        ));
+                    }
+                }
+            } else {
+                current_token.push(c);
+                match current_token.as_str() {
+                    "=>" => {
+                        tokens.push(Token::Implication);
+                        current_token.clear();
+                    }
+                    "<=" => {
+                        tokens.push(Token::ConverseImplication);
+                        current_token.clear();
+                    }
+                    _ => {
+                        return Err(ParseProgramError::InvalidToken(
+                            line.iter().collect(),
+                            current_token,
+                        ));
+                    }
+                }
+            }
+        }
+        Ok(tokens)
+    }
+
+    fn clean(tokens: Vec<Token>) -> Result<Vec<Token>, ParseProgramError> {
+        // TODO: don't add left and right parenthesis
+        let mut cleaned_tokens = vec![Token::LeftParenthesis];
+        let mut cnt_open = 0;
+        let mut cnt_implications = 0;
+
+        for token in tokens {
+            match token {
+                Token::LeftParenthesis => cnt_open += 1,
+                Token::RightParenthesis => {
+                    if cnt_open == 0 {
+                        return Err(ParseProgramError::UnbalancedParentheses);
+                    }
+                    cnt_open -= 1;
+                }
+                Token::Not if cleaned_tokens.last() == Some(&Token::Not) => {
+                    cleaned_tokens.pop();
+                }
+                _ => {
+                    cnt_implications += matches!(
+                        token,
+                        Token::Implication | Token::ConverseImplication | Token::Equivalence
+                    ) as u32;
+                    cleaned_tokens.push(token)
+                }
+            }
+        }
+
+        match cnt_implications {
+            0 => return Err(ParseProgramError::MissingImplication),
+            1 => {}
+            _ => return Err(ParseProgramError::MultipleImplications),
+        }
+
+        if cnt_open == 0 {
+            cleaned_tokens.push(Token::RightParenthesis);
+            Ok(cleaned_tokens)
+        } else {
+            Err(ParseProgramError::UnbalancedParentheses)
+        }
+    }
+
+    fn infix_to_rpn(tokens: Vec<Token>) -> Vec<Token> {
+        let mut output = Vec::new();
+        let mut operators = Vec::new();
+
+        for token in tokens {
+            match token {
+                Token::Fact(_) => output.push(token),
+                Token::Not | Token::LeftParenthesis => operators.push(token),
+                Token::RightParenthesis => {
+                    while operators.last() != Some(&Token::LeftParenthesis) {
+                        output.push(operators.pop().unwrap());
+                    }
+                    operators.pop();
+                    if let Some(Token::Not) = operators.last() {
+                        output.push(operators.pop().unwrap());
+                    }
+                }
+                Token::Equivalence
+                | Token::ConverseImplication
+                | Token::Implication
+                | Token::Xor
+                | Token::Or
+                | Token::And => {
+                    while operators.last().is_some_and(|top| {
+                        top != &Token::LeftParenthesis && top.precedence() >= token.precedence()
+                    }) {
+                        output.push(operators.pop().unwrap());
+                    }
+                    // TODO: handle NOT???
+                    operators.push(token);
+                }
+            }
+        }
+
+        while let Some(operator) = operators.pop() {
+            assert_ne!(operator, Token::LeftParenthesis);
+            output.push(operator);
+        }
+
+        output
+    }
+}
