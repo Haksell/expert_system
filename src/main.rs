598cbf8cb307254c7460f555f5be84e974feff47
Haksell <axel.brisse@gmail.com>
2026-06-05 00:38:22 +0200

    better print output + add test

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ mod rule;
 
 use crate::program::{ParseProgramError, Program};
 use clap::Parser;
+use itertools::Itertools as _;
 use std::{fs::File, io::BufReader, path::PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Troolean {
     False,
     Ambiguous,
@@ -23,7 +24,11 @@ fn main() -> Result<(), ParseProgramError> {
     let reader = BufReader::new(file);
     let mut program = Program::parse(reader)?;
     match program.solve() {
-        Some(results) => println!("{results:?}"),
+        Some(results) => {
+            for (fact, value) in results.iter().sorted() {
+                println!("{fact} is {value:?}");
+            }
+        }
         None => println!("There is a contradiction in the rules."),
     }
     Ok(())
